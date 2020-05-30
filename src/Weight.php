d12bf6eed18b249c41f74f420e921b702da67485
mansuang <mansuang@users.noreply.github.com>
2020-05-30 02:56:56 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -1,15 +1,13 @@
 <?php
 
 namespace Mansuang\UnitConversions;
-use DeepCopy\Exception;
 
 class Weight
 {
     private $kilograms;
 
-    public static function fromKilograms(float $kilograms): self {
-
-
+    public static function fromKilograms(float $kilograms): self
+    {
         return new static($kilograms);
     }
 
